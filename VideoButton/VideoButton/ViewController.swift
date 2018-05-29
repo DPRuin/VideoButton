931e4c7d941909cd0afdda6277cf9faaf9cf720e
mac126 <tulaomao152@126.com>
2018-05-29 12:01:54 +0800

    initial commit

---
+++ VideoButton/VideoButton/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  VideoButton
+//
+//  Created by mac126 on 2018/5/29.
+//  Copyright © 2018年 mac126. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        setupSlider()
+    }
+    
+    func setupSlider() {
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
