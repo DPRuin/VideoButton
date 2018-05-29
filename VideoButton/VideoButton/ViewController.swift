f15b8e1a3ae4ec6c3298c2f7d2f3f54dffea7f85
mac126 <tulaomao152@126.com>
2018-05-29 16:13:33 +0800

    录制按钮

--- VideoButton/VideoButton/ViewController.swift
+++ VideoButton/VideoButton/ViewController.swift
@@ -16,26 +16,15 @@ class ViewController: UIViewController {
     
     var count: Float!
     var timer: Timer!
+    let maxVideoTime: Float = 10.0
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.black
         // Do any additional setup after loading the view, typically from a nib.
-        setupSlider()
+        progressView.progress = 0
+        progressView.isHidden = true
         
-        squishButton.addTarget(self, action: #selector(self.squishButtonTouchUpInside(sender:)), for: UIControlEvents.touchUpInside)
-        
-        squishButton.addTarget(self, action: #selector(self.squishButtonTouchBegin(sender:)), for: UIControlEvents.touchDown)
-        squishButton.addTarget(self, action: #selector(self.squishButtonTouchEnd(sender:)), for: UIControlEvents.touchDragInside)
-        squishButton.addTarget(self, action: #selector(self.squishButtonTouchEnd(sender:)), for: UIControlEvents.touchDragOutside)
-        
-    }
-    
-    func setupSlider() {
-//        let inset: CGFloat = 10.0
-//        let y = view.bounds.height - inset
-//        let width = view.bounds.
-//        let rect = CGRect(x: inset, y: y, width: <#T##CGFloat#>, height: <#T##CGFloat#>)
-//        let slider = UISlider(frame: <#T##CGRect#>)
+        setupButtonGesture2()
     }
     
     override func didReceiveMemoryWarning() {
@@ -43,8 +32,42 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func setupButtonGesture2() {
+        squishButton.addTarget(self, action: #selector(self.squishButtonTouchUpInside(sender:)), for: UIControlEvents.touchUpInside)
+        let longPress = UILongPressGestureRecognizer(target: self, action: #selector(squishButtonLongPress(gesture:)))
+        // 定义长按0.8时间触发
+        longPress.minimumPressDuration = 0.8
+        squishButton.addGestureRecognizer(longPress)
+    }
+    
     @objc func squishButtonTouchUpInside(sender: UIButton) {
         print("squishButtonTouchUpInside")
+    }
+    
+    @objc func squishButtonLongPress(gesture: UILongPressGestureRecognizer) {
+
+        if gesture.state == .began {
+            print("longpressbegan")
+            
+            progressView.isHidden = false
+            
+            count = 0
+            timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(self.handleTimer(sender:)), userInfo: nil, repeats: true)
+            timer.fire()
+            
+        } else if gesture.state == .ended {
+            print("longpressended")
+            timer.invalidate()
+            print("-count-\(count)")
+        }
+        
+    }
+    
+    // MARK: - 从触发按钮到离开按钮的时间内（不固定）触发的事件
+    func setupButtonGesture3() {
+        squishButton.addTarget(self, action: #selector(self.squishButtonTouchBegin(sender:)), for: UIControlEvents.touchDown)
+        squishButton.addTarget(self, action: #selector(self.squishButtonTouchEnd(sender:)), for: UIControlEvents.touchUpInside)
+        squishButton.addTarget(self, action: #selector(self.squishButtonTouchEnd(sender:)), for: UIControlEvents.touchUpOutside)
     }
     
     @objc func squishButtonTouchBegin(sender: UIButton) {
@@ -62,7 +85,7 @@ class ViewController: UIViewController {
     
     @objc func handleTimer(sender: Timer) {
         count = count + 0.1
-        print("-count-\(count)")
+        progressView.progress = count / maxVideoTime
     }
 }
 
